1e0f96ad35279a9e372fbc5a0a76b205ad63de10
imanolortizfernandez <imanolof29@gmail.com>
2024-10-25 17:07:28 +0200

    optional int pipe

--- src/errors/errors.constants.ts
+++ src/errors/errors.constants.ts
@@ -3,3 +3,5 @@ export const CATEGORY_NOT_FOUND = 'Category not found'
 export const COMMENT_NOT_FOUND = 'Comment not found'
 export const EVENT_NOT_FOUND = 'Event not found'
 export const TICKET_NOT_FOUND = 'Ticket not found'
+
+export const OPTIONAL_INT_PIPE_NUMBER = '$key should be a number';
