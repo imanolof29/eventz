ea9f59ebae513e7ace6fec5bce4f34b608eb9a94
imanolortizfernandez <imanolof29@gmail.com>
2024-11-10 13:20:03 +0100

    missing import

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,13 +5,14 @@ import { AuthModule } from 'src/modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { S3Module } from 'src/providers/s3/s3.module';
+import { DeviceToken } from './deviceToken.entity';
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
         AuthModule,
-        TypeOrmModule.forFeature([User]),
+        TypeOrmModule.forFeature([User, DeviceToken]),
         S3Module
     ]
 })
